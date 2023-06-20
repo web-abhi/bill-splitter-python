81040727fb5527630d9a036f8891f67ced1f7154
Abhishek Singh <66420195+web-abhi@users.noreply.github.com>
2023-06-20 09:26:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ names = names_string.split(", ")
 x=len(names)
 choose = random.randint(0, x-1)
 person = names[choose] 
+#Here we can also use person= random.choice(names) for getting the same result without using multiple code of lines.
 print(person)
 
 
